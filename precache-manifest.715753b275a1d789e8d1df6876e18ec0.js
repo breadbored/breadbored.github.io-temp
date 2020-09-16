55206bcf72ee97982cc3a0ec2bd87cd3ae66d66f
jason <brad@identex.co>
2020-09-16 14:48:56 -0400

    Updates

---
+++ precache-manifest.715753b275a1d789e8d1df6876e18ec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194dc47a027eea220c40dc837283567e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba584b93622b0d138d7d",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "2c3f584eb103f5a2e5dd",
+    "url": "/static/css/main.0287e543.chunk.css"
+  },
+  {
+    "revision": "ba584b93622b0d138d7d",
+    "url": "/static/js/2.b72e1018.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b72e1018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3f584eb103f5a2e5dd",
+    "url": "/static/js/main.3af7489f.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
